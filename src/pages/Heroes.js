7a69fec578054cb93c533c29a8d79ec1484d731e
bbnagya <brad.bang@kakaocorp.com>
2019-03-30 18:23:51 +0900

    reactstrap 적용

---
+++ src/pages/Heroes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Heroes extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <div>Heroes</div>
+    )
+  }
+}
---
+++ src/pages/Scoreboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Scoreboard extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <div>Scoreboard</div>
+    )
+  }
+}
