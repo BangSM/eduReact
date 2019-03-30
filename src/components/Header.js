b27a43a2f1fb170909e44cb874cf13ce55dcc232
bbnagya <brad.bang@kakaocorp.com>
2019-03-30 10:52:42 +0900

    componentDidMount, componentWillUnmount

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Statistics} from './Statistics'
+import Stopwatch from './Stopwatch';
 
 export const Header = (props) => {
   const { title, totalPlayers, totalScore} = {...props}
@@ -8,6 +9,7 @@ export const Header = (props) => {
     <header>
       <Statistics totalPlayers={totalPlayers} totalScore={totalScore}/>
       <h1>{title}</h1>
+      <Stopwatch />
     </header>
   )
 }
---
+++ src/components/Stopwatch.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default class Stopwatch extends React.Component{
+  tickRef;
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      timer : 0,
+      isRunning : false
+    }
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      isRunning : !prevState.isRunning
+    }))
+  }
+
+  tick = () => {
+    this.state.isRunning ? this.setState(prevState => ({timer : prevState.timer+1})) : this.setState({})
+  }
+
+  // DOM이 렌더링 된 직후에 호출되는 라이프 사이클
+  // 3rd 라이브러리 로딩, 네트워크 호출
+  componentDidMount(){
+    this.tickRef = setInterval(this.tick, 1000)
+  }
+
+  // DOM이 파괴되기 직전에 호출되는 라이프 사이클
+  // 리소스 해제 등등
+  componentWillUnmount(){
+    clearInterval(this.tickRef)
+  }
+
+
+
+  render(){
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <span className="stopwatch-time">{this.state.timer}</span>
+        <button onClick={this.handleClick}>{this.state.isRunning ? 'Stop' : 'Start' }</button>
+        <button>Reset</button>
+      </div>
+    )
+  }
+}
