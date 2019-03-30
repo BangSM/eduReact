3c2f41206989116ec957348e9a982c804fd753ac
bbnagya <brad.bang@kakaocorp.com>
2019-03-30 11:32:58 +0900

    shouldComponentUpdate and PureComponent

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import Counter from './Counter'
 
-export const Player = (props) => {
-  return (
-  <div className="player">
-    <span className="player-name">
-      <button className="remove-player" onClick={()=> props.handleRemovePlayer(props.id)}>x</button>
-    </span>
-    <span className="player-name">{props.name}</span>
-    <Counter score={props.score} id={props.id} handleChangeScore={props.handleChangeScore}/>
-  </div>
-  )
+export default class Player extends React.PureComponent {
+  componentWillReceiveProps(nextProps, nextContext){
+    console.log('componentWillReceiveProps' , nextProps)
+  }
+  // shouldComponentUpdate(nextProps, nextState, nextContext){
+  //   //현재 스코어와 nextProps의 score를 비교
+  //   console.log('shouldComponentUpdate', nextProps)
+  //   return this.props.score !== nextProps.score ? true : false
+  // }
+  render(){
+    console.log(this.props.name, 'render')
+    return (
+    <div className="player">
+      <span className="player-name">
+        <button className="remove-player" onClick={()=> this.props.handleRemovePlayer(this.props.id)}>x</button>
+      </span>
+      <span className="player-name">{this.props.name}</span>
+      <Counter score={this.props.score} id={this.props.id} handleChangeScore={this.props.handleChangeScore}/>
+    </div>
+  )}
 }
